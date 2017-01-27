1893fc36f1a5a7f3a76e593d87b2e9b63fc2a759
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-01-26 21:16:31 -0800

    Add Queue class

--- W1D4/diy_adts.rb
+++ W1D4/diy_adts.rb
@@ -7,16 +7,37 @@ class Stack
   end
 
   def add(el)
-    @store << el
+    store << el
   end
 
   def remove
-    @store.pop
+    store.pop
   end
 
   def show
     store
   end
 
+end
+
+#Exercise 2 - Queue
+class Queue
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def enqueue(el)
+    store << el
+  end
+
+  def dequeue
+    store.shift
+  end
+
+  def show
+    store
+  end
 
 end
