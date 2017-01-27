6b6a80fbc9162c5212488e1dc5129d0feb3de3e1
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-01-26 21:31:50 -0800

    Add Map class

--- W1D4/diy_adts.rb
+++ W1D4/diy_adts.rb
@@ -39,5 +39,40 @@ class Queue
   def show
     store
   end
+end
+
+#Exercise 3 - Map
+class Map
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def assign(key, value)
+    keys = store.map { |pair| pair[0] }
+    if keys.include?(key)
+      index = keys.index(key)
+      store[index] = [key, value]
+    else
+      store << [key, value]
+    end
+  end
+
+  def lookup(key)
+    keys = store.map { |pair| pair[0] }
+    index = keys.index(key)
+    store[index]
+  end
+
+  def remove(key)
+    keys = store.map { |pair| pair[0] }
+    index = keys.index(key)
+    store.delete(store[index])
+  end
+
+  def show
+    store
+  end
 
 end
