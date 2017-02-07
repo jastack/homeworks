75d9c5167c9cae0fd9477bdf66d1e7f6aa3f2c47
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-02-07 09:46:41 -0800

    Edited homework

--- w3d2/plays_demo/plays.rb
+++ w3d2/plays_demo/plays.rb
@@ -11,6 +11,18 @@ class PlayDBConnection < SQLite3::Database
   end
 end
 
+class PlaywrightsDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('playwrights.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+
+
 class Play
   attr_accessor :title, :year, :playwright_id
 
@@ -48,4 +60,91 @@ class Play
         id = ?
     SQL
   end
+
+  def find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+
+    return nil if play.length.empty?
+
+    Play.new(play.first)
+  end
+
+  def find_by_playwright(name)
+    play = PlayDBConnection.instance.execut(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name
+    SQL
+    return nil if play.length.empty?
+
+    Play.new(play.first)
+  end
+end
+
+class Playwright
+  attr_accessor :name, :birth_year
+
+  def self.all
+    data = PlaywrightsDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlaywrightsDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlaywrightsDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays(name)
+    play = PlaywrightsDBConnection.instance.execut(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      JOIN
+        plays ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name
+    SQL
+    return nil if play.length.empty?
+
+    Playwright.new(play.first)
+  end
+
+
 end
